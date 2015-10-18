1385d39ddeb112a540345641fa597e8fa2d3008b
cephastsai <petertsai1994@gmail.com>
2015-10-18 15:22:43 +0800

    new
    new project

---
+++ 2D_Roguelik_game/Assets/Completed/Scripts/SceneManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+
+    public static bool menu_flag = true;
+    public static SceneManager instance = null;
+    // Use this for initialization
+    void Start () {
+        if (instance == null)
+        {
+            menu_flag = true;
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
