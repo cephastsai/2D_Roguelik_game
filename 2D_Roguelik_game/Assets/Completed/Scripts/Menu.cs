008d645915ee9566a6c049ba4e542b78a99a06a9
PAX100 <u960123@yahoo.com.tw>
2015-10-26 09:35:10 +0800

    33
    666

--- 2D_Roguelik_game/Assets/Completed/Scripts/Menu.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/Menu.cs
@@ -2,18 +2,45 @@
 using System.Collections;
 
 public class Menu : MonoBehaviour {
-
+	
     void OnGUI()
     {
         if (GUILayout.Button("Start Game"))
         {
             Application.LoadLevel("Main");
         }
+        /*if(GUI.Button(new Rect(0, 50, 50, 150), "Left"))
+		{
+			StartCoroutine("ChangeRuneScaleLeft");
+		}
+		if(GUI.Button(new Rect(590, 50, 50, 150), "Right")){
+			StartCoroutine("ChangeRuneScaleRight");
+			//Rune1 = GameObject.Find("Rune1(Clone)");
+			//Rune1.transform.position   += new Vector3(5,0.94f,0);
+			//Rune1.transform.localScale -= new Vector3(0.2f,0.2f,0.2f);
+
+		}*/
     }
 
     // Use this for initialization
-    void Start () {
-	
+	/*IEnumerator ChangeRuneScaleLeft() 
+	{
+		Rune1 = GameObject.Find("Rune1(Clone)");
+		Rune1.transform.position +=new Vector3(-5,0.94f,0);
+		Rune1.transform.localScale -= new Vector3 (0.2f, 0.2f, 0.2f);
+		yield return 0;
+	}
+	IEnumerator ChangeRuneScaleRight() 
+	{
+		Rune1 = GameObject.Find("Rune1(Clone)");
+		Rune1.transform.position   += new Vector3(5,0.94f,0);
+		Rune1.transform.localScale -= new Vector3(0.2f,0.2f,0.2f);
+		yield return 0;
+	}*/
+	void Start () {
+	//	GameObject  Prefab = Instantiate(Resources.Load("Rune1" ,typeof(GameObject)),RunePosition4,Quaternion.Euler(90, 180, 0)) as GameObject;
+
+
 	}
 	
 	// Update is called once per frame
