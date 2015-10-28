b115cfdd4455ad0c2219f52bf19c43e9b468e62e
shaou1206 <shaou1206@gmail.com>
2015-10-28 23:33:20 +0800

    20151028
    圖騰script讀取檔案。

--- 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
@@ -1,14 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.IO;
+using UnityEngine.UI;
 
 public class RuneManagerCs : MonoBehaviour {
 
 	private GameObject canvas = null;
 	private GameObject grid = null;
 
+    private FileInfo theSourceFile = null;
+    private StreamReader StreamReader = null;
+    private string text = " ";
+    private int i = 0;
+    private String[] textTemp = new String[50];
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 
 		//find gameobject "gird"
 		canvas = GameObject.Find("Canvas");
@@ -19,8 +28,17 @@ public class RuneManagerCs : MonoBehaviour {
 		// add component "SetRuneMaterial" ,all child
 		for(int temp=0;temp<grid.transform.childCount;temp++){
 			grid.transform.GetChild(temp).gameObject.AddComponent<SetRuneMaterial>().init(temp);
-		}
-	}
+        }
+        theSourceFile = new FileInfo("Assets/Completed/Resources/rune.txt");
+        StreamReader = theSourceFile.OpenText();
+        if (text != null)
+        {
+            text = StreamReader.ReadToEnd();
+            textTemp[i] = text;
+            Debug.Log("test:" + textTemp[i]);
+            i++;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
