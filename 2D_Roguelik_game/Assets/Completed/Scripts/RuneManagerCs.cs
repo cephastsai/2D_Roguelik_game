d65483bb960587b2404a27e2cca06d2e9b9a510a
cephastsai <petertsai1994@gmail.com>
2015-10-29 23:12:48 +0800

    偵測玩家是否有圖騰測試
    修改測試

--- 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
@@ -13,8 +13,8 @@ public class RuneManagerCs : MonoBehaviour {
     private StreamReader StreamReader = null;
     private string text = " ";
     private int i = 0;
-    private String[] RuneList = new String[50];
-
+    //private String[] RuneList = new String[50];
+	private String RuneList;
 
     // Use this for initialization
     void Start () {
@@ -26,14 +26,16 @@ public class RuneManagerCs : MonoBehaviour {
 		grid.transform.position = new Vector3(0,45,0);
 
 		// add component "SetRuneMaterial" ,all child
+		/*
 		for(int temp=0;temp<grid.transform.childCount;temp++){
+			print(RuneList[temp]);
 			if (RuneList [temp] == "1"){
-			grid.transform.GetChild(temp).gameObject.AddComponent<SetRuneMaterial>().init(temp);
+				grid.transform.GetChild(temp).gameObject.AddComponent<SetRuneMaterial>().init(temp);
 			}
 			else if (RuneList [temp] == "0"){
 				grid.transform.GetChild(temp).gameObject.AddComponent<SetNullRuneMaterial>().init(temp);
 			}
-        }
+        }*/
 
         //讀取檔案
         theSourceFile = new FileInfo("Assets/Completed/Resources/rune.txt");
@@ -41,13 +43,20 @@ public class RuneManagerCs : MonoBehaviour {
         if (text != null)
         {
             text = StreamReader.ReadToEnd();
-            RuneList[i] = text;
-            Debug.Log("test:" + RuneList[i]);
-            i++;
+            RuneList = text;
+            Debug.Log("test:" + RuneList);
         }
-		/*if (RuneList [0] == '1') {
 
-		}*/
+		// add component "SetRuneMaterial" ,all child
+		for(int temp=0;temp<grid.transform.childCount;temp++){
+			print(RuneList[temp]);
+			if (RuneList [temp] == '1'){
+				grid.transform.GetChild(temp).gameObject.AddComponent<SetRuneMaterial>().init(temp);
+			}
+			else if (RuneList [temp] == '0'){
+				grid.transform.GetChild(temp).gameObject.AddComponent<SetNullRuneMaterial>().init(temp);
+			}
+		}
     }
 	
 	// Update is called once per frame
