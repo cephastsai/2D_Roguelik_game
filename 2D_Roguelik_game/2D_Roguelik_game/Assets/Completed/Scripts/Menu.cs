514315371edf5bba1719f50b30f40ca7a66e6ae7
cephastsai <petertsai1994@gmail.com>
2015-10-31 08:49:46 +0800

    處理回主線問題
    重新覆蓋專案

---
+++ 2D_Roguelik_game/2D_Roguelik_game/Assets/Completed/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+
+	public GameObject RuneManager = null;
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("Start Game"))
+        {
+            Application.LoadLevel("Main");
+        }
+    }
+
+    // Use this for initialization
+
+	void Start () {
+		RuneManager = new GameObject("RuneManager");
+		RuneManager.AddComponent<RuneManagerCs>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ 2D_Roguelik_game/2D_Roguelik_game/Assets/Completed/Scripts/RamdomButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RamdomButton : MonoBehaviour {
+	public Texture RamdomImage = null;
+	// Use this for initialization\
+	void OnGUI()
+	{
+		if (GUI.Button (new Rect (10, 10, 50, 50), RamdomImage)) 
+		{
+
+		}
+	}
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
