194e337d139e4ac087bbd7033081928214630daa
PAX100 <u960123@yahoo.com.tw>
2015-11-01 22:22:44 +0800

    正確播放Animation
    頭痛死了...

--- 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
@@ -5,13 +5,11 @@ using System.IO;
 using UnityEngine.UI;
 
 public class RuneManagerCs : MonoBehaviour {
-
-	public Texture2D ButtonImage = null;
-	public GUISkin RandomSkin = null;
-
+	private Animator plane;
+	private Texture2D ButtonImage = null;
+	private GUISkin RandomSkin = null;
 	private GameObject canvas = null;
 	private GameObject grid = null;
-
     private FileInfo theSourceFile = null;
     private StreamReader StreamReader = null;
     private string text = " ";
@@ -19,7 +17,6 @@ public class RuneManagerCs : MonoBehaviour {
 	private int[] ListNo = new int[50];//總共的符文數量(預設50個)
 	private int j = 0;//玩家擁有幾個符文
 	private int CheckPoint = 0;//作為點選隨機按鈕的區隔數
-    //private String[] RuneList = new String[50];
 	private String RuneList;
 	private int K = 0;
 
@@ -27,42 +24,27 @@ public class RuneManagerCs : MonoBehaviour {
 		GUI.skin = RandomSkin;
 		if (GUI.Button(new Rect(520, 160,ButtonImage.width,ButtonImage.height),ButtonImage)&&CheckPoint==0)
 		{
+			plane.SetBool("Idle", false);
 			CheckPoint +=1;
 			for(int temp=0;temp<grid.transform.childCount;temp++){
 				if (RuneList [temp] == '1'){
 					ListNo[j] = temp;
 					j++;
-					//K = new int(Random.Range(1,j));
 				}
 			}
-            K = UnityEngine.Random.Range(0, j);
-			print(j);//顯示擁有幾個符文
+            K = UnityEngine.Random.Range(1, j);
             print(ListNo[K]); //隨機產生數字
 		}
 		
 	}
     // Use this for initialization
     void Start () {
+		plane = GameObject.Find("plane").GetComponent<Animator>();
 		ButtonImage = (Texture2D)Resources.Load("Image/Random");
 		RandomSkin =  (GUISkin)Resources.Load("GUISkin/RandomButton");
-		//find gameobject "gird"
 		canvas = GameObject.Find("Canvas");
 		grid = canvas.transform.GetChild(1).GetChild(0).gameObject;
-
 		grid.transform.position = new Vector3(0,45,0);
-
-		// add component "SetRuneMaterial" ,all child
-		/*
-		for(int temp=0;temp<grid.transform.childCount;temp++){
-			print(RuneList[temp]);
-			if (RuneList [temp] == "1"){
-				grid.transform.GetChild(temp).gameObject.AddComponent<SetRuneMaterial>().init(temp);
-			}
-			else if (RuneList [temp] == "0"){
-				grid.transform.GetChild(temp).gameObject.AddComponent<SetNullRuneMaterial>().init(temp);
-			}
-        }*/
-
         //讀取檔案
         theSourceFile = new FileInfo("Assets/Completed/Resources/rune.txt");
         StreamReader = theSourceFile.OpenText();
@@ -72,8 +54,6 @@ public class RuneManagerCs : MonoBehaviour {
             RuneList = text;
             Debug.Log("test:" + RuneList);
         }
-
-		// add component "SetRuneMaterial" ,all child
 		for(int temp=0;temp<grid.transform.childCount;temp++){
 			print(RuneList[temp]);
 			if (RuneList [temp] == '1'){
