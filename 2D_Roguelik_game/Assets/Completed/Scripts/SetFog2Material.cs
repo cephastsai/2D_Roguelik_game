821d9d824e08f5712dcdb2c15b6fc22f29f9ddfc
cephastsai <petertsai1994@gmail.com>
2015-11-04 16:05:06 +0800

    取消註解

--- 2D_Roguelik_game/Assets/Completed/Scripts/SetFog2Material.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/SetFog2Material.cs
@@ -2,13 +2,13 @@
 using System.Collections;
 
 public class SetFog1Material : MonoBehaviour {
-	//public Renderer Rend = null;
+	public Renderer Rend = null;
 	public Material Mat = null;
 	// Use this for initialization
 	void Start () {
-		//Rend = GetComponent<Renderer>();
+		Rend = GetComponent<Renderer>();
 		Mat = (Material)Resources.Load("Material/Fog2",typeof(Material));
-		//Rend.sharedMaterial = Mat;
+		Rend.sharedMaterial = Mat;
 		Mat.shader = Shader.Find("Particles/Additive");
 		Mat.mainTexture = (Texture)Resources.Load("Image/Fog02",typeof(Texture));
 		
