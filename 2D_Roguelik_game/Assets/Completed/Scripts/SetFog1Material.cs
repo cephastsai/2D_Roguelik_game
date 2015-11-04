f98a7c8cc7cd4e9dd3a6c62c631a075735306b03
PAX100 <u960123@yahoo.com.tw>
2015-11-04 14:45:45 +0800

    修正迷霧載入材質問題

---
+++ 2D_Roguelik_game/Assets/Completed/Scripts/SetFog1Material.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetFog2Material : MonoBehaviour {
+	public Renderer Rend = null;
+	public Material Mat = null;
+	// Use this for initialization
+	void Start () {
+		Rend = GetComponent<Renderer>();
+		Mat = (Material)Resources.Load("Material/Fog1",typeof(Material));
+		Rend.sharedMaterial = Mat;
+		Mat.shader = Shader.Find("Particles/Additive");
+		Mat.mainTexture = (Texture)Resources.Load("Image/Fog01",typeof(Texture));
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
