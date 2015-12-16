bd76bc652737e514fde2a9e7d3a8ea3040c9755d
shaou1206 <shaou1206@gmail.com>
2015-12-16 21:18:03 +0800

    20151216_scene&position

--- 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
+++ 2D_Roguelik_game/Assets/Completed/Scripts/RuneManagerCs.cs
@@ -27,14 +27,84 @@ public class RuneManagerCs : MonoBehaviour {
     private static Animator aniController;
 
     void Awake(){
-        StartGame = (Texture2D)Resources.Load("Image/StartGame");
+        //StartGame = (Texture2D)Resources.Load("Image/StartGame");
 		//RandomSkin =  (GUISkin)Resources.Load("GUISkin/RandomButton");
+        //GameObject.Find("StartGame").SetActive()
 	}
 
-	void OnGUI()
+    public void random()
     {
-        //點擊圖片按鈕測試
-        if (GUI.Button(new Rect(30, 40, StartGame.width, StartGame.height), StartGame) && CheckPoint == 0)
+        CheckPoint += 1;
+        //plane.SetBool("Idle", false);
+        for (int temp = 0; temp < grid.transform.childCount; temp++)
+        {
+            if (RuneList[temp] == '1')
+            {
+                ListNo[j] = temp;
+                j++;
+                //K = new int(Random.Range(1,j));
+            }
+        }
+        K = UnityEngine.Random.Range(0, j);
+
+        GameObject RuneRandom = new GameObject("RuneRandom");
+        RuneRandom.AddComponent<MeshRenderer>();
+        RuneRandom.AddComponent<MeshFilter>();
+        RuneRandom.AddComponent<Animator>();
+
+        print("RandomNum:" + (ListNo[K] + 1)); //隨機產生數字
+
+        SceneManager.CurrentRuneID = K + 1;
+        Story.rune = K + 1;
+
+        //隨機圖騰置中
+        canvas = GameObject.Find("Canvas");
+        startRune = canvas.transform.GetChild(1).GetChild(0).GetChild(K).gameObject;
+
+        aniController = canvas.transform.GetChild(1).GetChild(0).GetChild(K).gameObject.GetComponent<Animator>();
+
+        if (K == 0)
+        {
+            grid.transform.position = new Vector3(-116, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+        else if (K == 1)
+        {
+            grid.transform.position = new Vector3(-50, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+        else if (K == 2)
+        {
+            grid.transform.position = new Vector3(15, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+        else if (K == 3)
+        {
+            grid.transform.position = new Vector3(80, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+        else if (K == 4)
+        {
+            grid.transform.position = new Vector3(137, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+        else if (K == 5)
+        {
+            grid.transform.position = new Vector3(200, 45, 0);
+            aniController.SetInteger("Number", 1);
+            StartCoroutine("EnterLevel");
+        }
+    }
+
+	/*void OnGUI()
+    {
+        點擊圖片按鈕測試
+        if (GUI.Button(new Rect(0, 0, StartGame.width, StartGame.height), StartGame) && CheckPoint == 0)
         {
             CheckPoint += 1;
             //plane.SetBool("Idle", false);
@@ -106,9 +176,9 @@ public class RuneManagerCs : MonoBehaviour {
                 grid.transform.position = new Vector3(260, 37, 0);
                 aniController.SetInteger("Number", 1);
                 StartCoroutine("EnterLevel");
-            }*/
+            }
         }
-    }
+    }*/
 
     IEnumerator EnterLevel()
     {
@@ -120,11 +190,11 @@ public class RuneManagerCs : MonoBehaviour {
     // Use this for initialization
     void Start () {
 
-
+        
 		//find gameobject "gird"
 		canvas = GameObject.Find("Canvas");
 		grid = canvas.transform.GetChild(1).GetChild(0).gameObject;
-		grid.transform.position = new Vector3(0,37,0);
+		grid.transform.position = new Vector3(0,45,0);
 
         // add component "SetRuneMaterial" ,all child
         /*
---
+++ 2D_Roguelik_game/Assets/Completed/Scripts/StartGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartGame : MonoBehaviour {
+
+    public GameObject RuneManager;
+
+    void OnMouseDown()
+    {
+        RuneManager = GameObject.Find("RuneManager");
+        RuneManager.GetComponent<RuneManagerCs>().random();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
