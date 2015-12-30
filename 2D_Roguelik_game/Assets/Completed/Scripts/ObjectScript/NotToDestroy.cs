a33b52dd405418524f8a20845e6b141ce96a45ab
cephastsai <petertsai1994@gmail.com>
2015-12-30 13:06:01 +0800

    處理遊戲介面轉場

---
+++ 2D_Roguelik_game/Assets/Completed/Scripts/ObjectScript/NotToDestroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NotToDestroy : MonoBehaviour {
+
+	public static GameObject UIobject;
+
+	void Awake(){
+		if(UIobject == null){
+			
+			UIobject = gameObject;
+			DontDestroyOnLoad(gameObject);
+			
+		}else if(UIobject != gameObject){
+			
+			Destroy(gameObject);
+		}
+	}
+
+}
